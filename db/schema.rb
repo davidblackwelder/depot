e5e69b9ce914b5c7ded798f6080a960d777a9f80
David Blackwelder <github@davidblackwelder.com>
2021-03-13 09:58:12 -0500

    Created a cart

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_12_190211) do
+ActiveRecord::Schema.define(version: 2021_03_13_145003) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
